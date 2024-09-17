2d7b18df029eb5d829fff18cdd973014ba7a7c3e
Hardik <hardik@Neerajs-MacBook-Air.local>
2024-09-17 18:10:02 +0530

    1st mysql connection

---
+++ get_blog.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const { createPool } = require('mysql');
+const app = express();
+const port = 9000;
+const cors = require('cors');
+app.use(cors());
+
+const pool = createPool({
+    host: "localhost",
+    user: "root",
+    password: "your_password",
+    database: "hardik",
+    connectionLimit: 10
+})
+
+app.get('/api/blogs', (req, res) => {
+    pool.query(`SELECT * FROM blogs`, (err, result) => {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+        res.json(result);
+    });
+});
+
+app.listen(port, () => {
+    console.log(`server running on localhost ${port}`);
+});
