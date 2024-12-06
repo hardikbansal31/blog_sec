016644f499b03d70ab13cfee663de0b7e6f5f7bd
Hardik <hardik@Neerajs-MacBook-Air.local>
2024-12-06 19:19:35 +0530

    sesarch css

--- get_blog.js
+++ get_blog.js
@@ -25,3 +25,19 @@ app.get('/api/blogs', (req, res) => {
 app.listen(port, () => {
     console.log(`server running on localhost ${port}`);
 });
+
+
+app.get('/api/blogs/search', (req, res) => {
+    const { title, auth } = req.query;
+    if (!title) {
+        return res.status(400).json({ error: "title not found" });
+    }
+    const q = `SELECT * FROM blogs WHERE (title) = (?)`;
+
+    pool.query(q, [title], (err, result) => {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+        res.status(200).json({ message: 'blog retrieved on search pg', blogID: result.insertId });
+    });
+});
